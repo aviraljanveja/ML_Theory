fdec7f4869b6014c322603c1ced1ba9b7b5deaac
Aviral Janveja <aviraljanveja@gmail.com>
2023-08-11 18:58:25 +0200

    Delete Python directory

--- Python/HelloWorld/1 - Print.py
+++
@@ -1,5 +0,0 @@
-print("Can I Think?")
-# This is my first line of code in Python.
-# This is a Python comment.
-# Anything between quotation marks is a String.
-# A String in programming is a series of Characters.
--- Python/HelloWorld/2 - Expression.py
+++
@@ -1,5 +0,0 @@
-print('*' * 10)
-# This here is an expression; A piece of code that produces a value.
-# Here we multiply the String by 10.
-# When Python interpreter tries to execute this line,
-# It evaluates the expression - which prints 10 asterisks(*).
